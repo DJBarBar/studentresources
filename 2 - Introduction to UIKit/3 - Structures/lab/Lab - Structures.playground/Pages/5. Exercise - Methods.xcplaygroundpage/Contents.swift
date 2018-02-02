b9a814d205c3e556c55e0c250dc395185e34fc6e
Cory Barton <coryjbarton@gmail.com>
2018-02-02 10:26:00 -0700

    Completed pages 1-4, Minor changes to page 5. 2.3 Structures

--- 2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -8,7 +8,13 @@ struct Book {
     var author: String
     var pages: Int
     var price: Double
+    
+    func description() {
+        print("Title: \(title), Author: \(author), Pages: \(pages), Price: \(price)")
+    }
 }
+let myBook = Book(title: "Angels and Demons", author: "Dan Brown", pages: 616, price: 9.99)
+let description = myBook.description()
 /*:
  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
  */
