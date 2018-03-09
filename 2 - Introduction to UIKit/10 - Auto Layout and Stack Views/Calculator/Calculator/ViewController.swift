1a9a6bc9a8df6db327f600f5b4f817ce7d0ffa1d
Cory Barton <coryjbarton@gmail.com>
2018-03-09 08:21:24 -0700

    Minor changes and fixes

--- 2 - Introduction to UIKit/10 - Auto Layout and Stack Views/Calculator/Calculator/ViewController.swift
+++ 2 - Introduction to UIKit/10 - Auto Layout and Stack Views/Calculator/Calculator/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var currentOperator =
+    var 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        reset()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func reset() {
+        displayLabel.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    @IBAction func numberTapped(_ sender: UIButton) {
+        guard let number = sender.currentTitle else {
+            return
+        }
+        self.displayLabel.text = displayLabel.text ?? "" + number
+    }
+    
+    func getLabelCurrentText() -> String {
+        let currentText = displayLabel.text ?? ""
+        if currentText == "0" {
+            return ""
+        } else {
+            return currentText
+        }
+    }
+    
+    @IBAction func zeroTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func dotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func computationTapped(_ sender: UIButton) {
+        currentOperator = sender.currentTitle
+        guard let displayText = displayLabel.text, let firstNumber = Double(displayText) else {
+            return
+        }
+        
+        self.firstNumber = firstNumber
+    }
+    
+    @IBAction func equalTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+        reset()
+    }
+    
+    @IBAction func negativePositiveTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func percentTapped(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
