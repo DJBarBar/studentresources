64ae600f204e366974bab561b69ac79dba62d753
Cory Barton <coryjbarton@gmail.com>
2018-03-19 09:28:55 -0600

    Tab Bar Lab

---
+++ 3 - Navigation and Workflows/7 - Tab Bar Controllers/AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Cory Barton on 3/19/18.
+//  Copyright © 2018 Cory Barton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
