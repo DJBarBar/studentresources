8f0b00a45f9ea15554edcf8e95554546fb2f17c3
Cory Barton <coryjbarton@gmail.com>
2018-03-26 09:43:06 -0600

    Completed Page 2 of Protocols Lab

--- 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/2. App Exercise - Printable Workouts.xcplaygroundpage/Contents.swift
+++ 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/2. App Exercise - Printable Workouts.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@
  
  The `Workout` objects you have created so far in app exercises don't show a whole lot of useful information when printed to the console. They also aren't very easy to compare or sort. Throughout these exercises, you'll make the `Workout` class below adopt certain protocols that will solve these issues.
  */
-class Workout: CustomStringConvertible, Equatable, Comparable {
+class Workout: CustomStringConvertible, Equatable, Comparable, Codable {
     var distance: Double
     var time: Double
     var identifier: Int
@@ -47,11 +47,18 @@ print(myWorkout == wifeWorkout)
  */
 let dadWorkout = Workout(distance: 0, time: 0, identifier: 3)
 let momWorkout = Workout(distance: 1, time: 1, identifier: 4)
-let sisterWorkout = Workout(distance: <#T##Double#>, time: <#T##Double#>, identifier: <#T##Int#>)
+let sisterWorkout = Workout(distance: 9, time: 45, identifier: 5)
+let workouts = [myWorkout, wifeWorkout, dadWorkout, momWorkout, sisterWorkout]
+let sortedWorkouts = workouts.sorted(by: >)
+print(workouts)
+print(sortedWorkouts)
 /*:
  Make `Workout` adopt the `Codable` protocol so you can easily encode `Workout` objects as data that can be stored between app launches. Use a `JSONEncoder` to encode one of your `Workout` instances. Then use the encoded data to initialize a `String`, and print it to the console.
  */
 import Foundation
-
+let jsonEncoder = JSONEncoder()
+if let jsonData = try? jsonEncoder.encode(myWorkout), let jsonString = String(data: jsonData, encoding: .utf8) {
+    print(jsonString)
+}
 
 //: [Previous](@previous)  |  page 2 of 5  |  [Next: Exercise - Create a Protocol](@next)
